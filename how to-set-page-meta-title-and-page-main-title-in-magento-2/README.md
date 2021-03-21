8d31b1471d29f0532b2a3bcaca9c8019621f67a7
Alaa Al-Maliki <alaa.almaliki@gmail.com>
2021-03-21 01:29:48 +0000

    How to set page meta title and page main title in Magento 2

---
+++ how to-set-page-meta-title-and-page-main-title-in-magento-2/README.md
@@ -0,0 +1,3 @@
+# How to set page meta title and page main title in Magento 2
+
+[Youtube Tutorial](https://youtu.be/OJKopQ8oc9o)
---
+++ how to-set-page-meta-title-and-page-main-title-in-magento-2/app/code/Training/PageTitleExample/Controller/Index/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\PageTitleExample\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected PageFactory $pageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        $page = $this->pageFactory->create();
+        $page->getConfig()->getTitle()->set('Page Title Example');
+        $page->getLayout()->getBlock('page.main.title')->setPageTitle('Magento 2 Training set from Controller');
+        return $page;
+    }
+}
