50cda416c69268f2a9334ed8983374d356a57324
Alaa Al-Maliki <alaa.almaliki@gmail.com>
2021-03-28 02:51:41 +0100

    All you need to know about basic system configuration in Magento 2

---
+++ all-you-need-to-know-about-basic-system-configurations-in-magento-2/README.md
@@ -0,0 +1,3 @@
+# All you need to know about basic system configuration in Magento 2
+
+[Youtube Tutorial](https://youtu.be/MDDEyCqxtRE)
---
+++ all-you-need-to-know-about-basic-system-configurations-in-magento-2/app/etc/env.php
@@ -0,0 +1,95 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '6c744747650f0e2b102081f436cefa9a'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento_site',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '196_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '196_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'magento-site.test'
+    ],
+    'install' => [
+        'date' => 'Mon, 09 Nov 2020 00:58:12 +0000'
+    ],
+    'system' => [
+        'default' => [
+            'web' => [
+                'secure' => [
+                    'base_url' => 'https://magento-site.test/'
+                ],
+                'unsecure' => [
+                    'base_url' => 'https://magento-site.test/'
+                ],
+            ]
+        ]
+    ]
+];
